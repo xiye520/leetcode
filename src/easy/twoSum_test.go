4749c87e8b62a589180d3428cd5412a404ac34ae
xiye <xiye518@gmail.com>
2018-09-26 17:23:22 +0800

    init

---
+++ src/easy/twoSum_test.go
@@ -0,0 +1,44 @@
+package easy
+
+import "testing"
+
+/*
+给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+
+你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+*/
+
+func twoSum(nums []int, target int) []int {
+	var indexs []int
+	//nmap := make(map[int]int, 0)
+	//for k, v := range nums {
+	//	nmap[k] = v
+	//}
+
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				indexs = append(indexs, i, j)
+				break
+			}
+		}
+	}
+
+	return indexs
+}
+
+func TestTwoSum(t *testing.T) {
+	target := 9
+	nums := []int{2, 7, 11, 15}
+	indexs := twoSum(nums, target)
+	if len(nums) < 2 || nums[indexs[0]]+nums[indexs[1]] != target {
+		t.Error("fails....")
+	}
+}
