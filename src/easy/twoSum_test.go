62a870ac46d042f7caf506d032bc57328df86a54
xiye <xiye518@gmail.com>
2018-09-26 17:27:21 +0800

    init

--- src/easy/twoSum_test.go
+++ src/easy/twoSum_test.go
@@ -34,6 +34,7 @@ func TestTwoSum(t *testing.T) {
 	target := 9
 	nums := []int{2, 7, 11, 15}
 	indexs := twoSum(nums, target)
+
 	if len(nums) < 2 || nums[indexs[0]]+nums[indexs[1]] != target {
 		t.Error("fails....")
 	}
