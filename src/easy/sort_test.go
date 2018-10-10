bec3dde4303150a11d3d4c2d57794d2caad82759
xiye <xiye518@gmail.com>
2018-10-10 23:49:49 +0800

    golang风格 精简代码

--- src/easy/sort_test.go
+++ src/easy/sort_test.go
@@ -11,11 +11,7 @@ func BubbleSort(array []int) []int {
 	for i := 0; i < len(array); i++ {
 		for j := 0; j < len(array)-1-i; j++ {
 			if array[j] > array[j+1] {
-				//array[j], array[j+1] = array[j+1], array[j]
-				temp := array[j]
-				array[j] = array[j+1]
-				array[j+1] = temp
-
+				array[j], array[j+1] = array[j+1], array[j]
 			}
 		}
 	}
