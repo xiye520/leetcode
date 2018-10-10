a2e45f83e0c5cba395d5b25a409bf624dba303d3
xiye <xiye518@gmail.com>
2018-10-10 23:43:25 +0800

    add selectSort and bubbleSort

---
+++ src/easy/sort_test.go
@@ -0,0 +1,54 @@
+package easy
+
+import (
+	"fmt"
+	"testing"
+)
+
+var Arr = []int{12, 84, 5, 63, 37, 9, 56, 1399}
+
+//冒泡排序，向后移动，从小到大排序,其思想为相邻两个数进行比较，将较大的滞后，时间复杂度O(N^2
+func BubbleSort(array []int) []int {
+	//fmt.Println("befor sort arr:", arr)
+	for i := 0; i < len(array); i++ {
+		for j := 0; j < len(array)-1-i; j++ {
+			if array[j] > array[j+1] {
+				//array[j], array[j+1] = array[j+1], array[j]
+				temp := array[j]
+				array[j] = array[j+1]
+				array[j+1] = temp
+
+			}
+		}
+	}
+	//fmt.Println("after sort arr:", arr)
+
+	return array
+}
+func TestBubbleSort(t *testing.T) {
+	array := []int{12, 84, 5, 63, 37, 9, 56, 1399}
+	fmt.Println("befor sort array:", array)
+	BubbleSort(array)
+	fmt.Println("after sort array:", array)
+}
+
+//选择排序，从后面找个最小的放在前面的位置，从小到大排序,时间复杂度O(N^2)
+func SelectSort(array []int) []int {
+
+	for i := 0; i < len(array)-1; i++ {
+		for j := i + 1; j < len(array); j++ {
+			if array[i] > array[j] {
+				array[i], array[j] = array[j], array[i]
+			}
+		}
+	}
+
+	return array
+}
+
+func TestSelectSort(t *testing.T) {
+	array := []int{12, 84, 5, 63, 37, 9, 56, 1399}
+	fmt.Println("befor sort array:", array)
+	SelectSort(array)
+	fmt.Println("after sort array:", array)
+}
