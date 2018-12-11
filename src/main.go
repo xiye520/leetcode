538148a1cd28aa5c74665db1c8d52fa5c9c195ea
xiye <xiye518@gmail.com>
2018-12-12 01:12:28 +0800

    8. 字符串转换整数 (atoi)

--- src/main.go
+++ src/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("hello")
+
+	str := "-056546161321615612315"
+	n, err := strconv.Atoi(str)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("str: %s转换为数字后为：%d\n", str, n)
 }
