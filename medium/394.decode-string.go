654bb566c65b395e42d2bdf42875fe3c68b45544
xiye <xiye518@gmail.com>
2019-11-11 21:41:41 +0800

    add 394 solution

---
+++ medium/394.decode-string.go
@@ -0,0 +1,61 @@
+package medium
+
+import (
+	"bytes"
+	"strconv"
+	"unicode"
+)
+
+/*
+给定一个经过编码的字符串，返回它解码后的字符串。
+
+编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
+
+你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。
+
+此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
+
+示例:
+
+s = "3[a]2[bc]", 返回 "aaabcbc".
+s = "3[a2[c]]", 返回 "accaccacc".
+s = "2[abc]3[cd]ef", 返回 "abcabccdcdcdef".
+
+链接：https://leetcode-cn.com/problems/decode-string
+*/
+type Pair struct {
+	nextNum int
+	thisStr string
+}
+
+func multiplyStr(n int, s string) string {
+	var buffer bytes.Buffer
+	for i := 0; i < n; i++ {
+		buffer.WriteString(s)
+	}
+	return buffer.String()
+}
+
+func decodeString(s string) string {
+	num := 0
+	curStr := ""
+	stack := []Pair{}
+
+	for _, c := range s {
+		if unicode.IsDigit(c) { // end of string
+			n, _ := strconv.Atoi(string(c))
+			num = num*10 + n
+		} else if c == '[' { // end of number
+			stack = append(stack, Pair{num, curStr})
+			num, curStr = 0, ""
+		} else if c == ']' {
+			pair := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			curStr = pair.thisStr + multiplyStr(pair.nextNum, curStr)
+		} else {
+			curStr = curStr + string(c)
+		}
+	}
+
+	return curStr
+}
--- medium/8_string-to-integer-atoi.go
+++ medium/8_string-to-integer-atoi.go
@@ -1,8 +1,10 @@
 package medium
 
 import (
+	"math"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 /*
@@ -129,8 +131,52 @@ func myAtoi(str string) int {
 	return int(n)
 }
 
-func myAtoi2(str string) int {
-	//reg := regexp.MustCompile(".+?([0-9]+).+?")
-	//result := reg.FindAllString(str, -1)
-	return 0
+func myAtoi2(s string) int {
+	return convert(clean(s))
+}
+
+func clean(s string) (sign int, abs string) {
+	// 除去首尾的空格
+	s = strings.TrimSpace(s)
+	if s == "" {
+		return
+	}
+
+	switch s[0] {
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		sign, abs = 1, s
+	case '+':
+		sign, abs = 1, s[1:]
+	case '-':
+		sign, abs = -1, s[1:]
+	default:
+		abs = ""
+		return
+	}
+
+	for i, b := range abs {
+		if b < '0' || '9' < b {
+			abs = abs[:i]
+			break
+		}
+	}
+
+	return
+}
+
+func convert(sign int, absStr string) int {
+	abs := 0
+
+	for _, b := range absStr {
+		abs = abs*10 + int(b-'0')
+		// 检查溢出
+		switch {
+		case sign == 1 && abs > math.MaxInt32:
+			return math.MaxInt32
+		case sign == -1 && sign*abs < math.MinInt32:
+			return math.MinInt32
+		}
+	}
+
+	return sign * abs
 }
